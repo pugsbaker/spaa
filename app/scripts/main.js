c8b4c91e08232baf4cb755f90b243236d3a1ee3b
Christian Baker <cbaker@christianbaker.com.au>
2015-02-09 10:35:55 +1100

    words slides mostly done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,23 @@
 	$.get('templates/main.mustache', function(template) {
 		var rendered = Mustache.render(template, self.data);
 	    $('#target').html(rendered);
+	    $(".background").first().addClass( 'active' );
 	});
+	
+	
+	setInterval(function() {
+		self.changeSlide();
+	}, 25000);
+	
+	this.changeSlide = function() {
+		var activeSlide = $(".background.active");
+		var isLast = $(".background").last().hasClass( 'active' );
+		activeSlide.removeClass( 'active');
+		if(isLast) {
+			$(".background").first().addClass( 'active' );
+		} else {
+			activeSlide.next().addClass( 'active' );
+		}
+	}
 	
 })();
