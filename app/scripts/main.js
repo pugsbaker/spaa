35a8dc80078d7410b3c7dd9a55c4616d04473ac1
Christian Baker <cbaker@christianbaker.com.au>
2015-02-09 11:29:33 +1100

    js changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,34 @@
-
+'use strict';
 (function() {
-	this.data;
+	
+	$(document).ready(function() {
+		var self = this;
+		$.getJSON( 'data/data.json', function(result){
+			self.data = result;
+		});
 
-	$.getJSON( 'data/data.json', function(result){
-		self.data = result;
-	})
-
-	$.get('templates/main.mustache', function(template) {
-		var rendered = Mustache.render(template, self.data);
-	    $('#target').html(rendered);
-	    $(".background").first().addClass( 'active' );
+		$.get('templates/main.mustache', function(template) {
+			var rendered = Mustache.render(template, self.data);
+		    $('#target').html(rendered);
+		    $('.background').first().addClass( 'active' );
+		});
+		
+		
+		setInterval(function() {
+			self.changeSlide();
+		}, 25000);
+		
+		this.changeSlide = function() {
+			var activeSlide = $('.background.active');
+			var isLast = $('.background').last().hasClass( 'active' );
+			activeSlide.removeClass( 'active');
+			if(isLast) {
+				$('.background').first().addClass( 'active' );
+			} else {
+				activeSlide.next().addClass( 'active' );
+			}
+		};
 	});
 	
-	
-	setInterval(function() {
-		self.changeSlide();
-	}, 25000);
-	
-	this.changeSlide = function() {
-		var activeSlide = $(".background.active");
-		var isLast = $(".background").last().hasClass( 'active' );
-		activeSlide.removeClass( 'active');
-		if(isLast) {
-			$(".background").first().addClass( 'active' );
-		} else {
-			activeSlide.next().addClass( 'active' );
-		}
-	}
 	
 })();
